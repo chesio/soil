40d28db324e95a60ff6b9cbc545387a5b5536aea
Česlav Przywara <cp@bluechip.at>
2025-10-24 13:38:34 +0200

    Use date-based versioning and push new version

--- soil.php
+++ soil.php
@@ -4,7 +4,7 @@
  * Plugin Name:        Soil
  * Plugin URI:         https://github.com/chesio/soil/
  * Description:        A collection of modules to apply theme-agnostic front-end modifications to WordPress.
- * Version:            4.1.1.pl
+ * Version:            2025.10.24
  * Author:             Roots
  * Author URI:         https://roots.io/
  * GitHub Plugin URI:  https://github.com/chesio/soil/
